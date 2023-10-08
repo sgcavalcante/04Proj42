e3d430af17c93a029f3ac715de25ff638f6983e5
Sergio Cavalcante <sgcdani@gmail.comc>
2023-10-08 14:43:10 -0300

    adicionado pagina para criar usuraios

--- root/settings.py
+++ root/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = str(os.getenv('CHAVE_SECRETA'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-3c8ea.up.railway.app/','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3c8ea.up.railway.app/']
+#CSRF_TRUSTED_ORIGINS = ['https://web-production-3c8ea.up.railway.app/']
 # Application definition
 
 INSTALLED_APPS = [
