35ccee1065c379d587505f6c0ce2d64166545d6a
Sergio Cavalcante <sgcdani@gmail.comc>
2023-10-07 23:33:18 -0300

    alteração para sqlite

--- root/settings.py
+++ root/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app01',
+    'app01.apps.App01Config', #ou apenas app01
 ]
 
 MIDDLEWARE = [
