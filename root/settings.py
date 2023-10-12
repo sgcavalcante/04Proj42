9ae7300b58db4b1eaec381dc41da9473036cd856
Sergio Cavalcante <sgcdani@gmail.comc>
2023-10-12 07:16:48 -0300

    atualizando requirements.txt

--- root/settings.py
+++ root/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 #    }
 #}
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -98,7 +98,7 @@ DATABASES = {
         'PORT': '3306',       # Porta padrão do MySQL
     }
 }
-
+'''
 
 
 # Password validation
