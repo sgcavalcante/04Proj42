01dd15400db2d3eb74c36432d830d14135158bc2
Sergio Cavalcante <sgcdani@gmail.comc>
2023-10-12 09:40:15 -0300

    add mysql

--- root/settings.py
+++ root/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path,os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,6 +88,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 #    }
 #}
 
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 '''
 DATABASES = {
     'default': {
